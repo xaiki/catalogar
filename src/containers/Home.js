b0764e7aba7dc2b86d35031a3d7c228ca383279d
Niv Sardi <xaiki@evilgiggle.com>
2018-10-19 14:07:54 -0300

    vote on gun
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,27 +1,50 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
 import keydown from 'react-keydown'
+import gun, { gunGet } from '../gun'
 //
 
 class Main extends React.PureComponent {
   state = {
-    idx: 0
+    idx: 0,
+    img: this.props.images[0]
   }
   @keydown('1', '2', '3', '4')
   onKeyPress({key}) {
-    console.error('keypress', key)
-    this.setState(({idx}) => ({
-      idx: idx + 1
-    }))
+    const {img, idx} = this.state
+    gun.get(img).once((data) => {
+      data = data || {}
+      gun.put({
+        [img]: {
+          ...data,
+          [key]: (data[key] || 0) + 1
+        }
+      })
+    })
+
+    const nextIdx = idx + 1
+    const nextImg = this.props.images[nextIdx]
+    gun.get(nextImg).once(votes => {
+      // remove gunisms
+      delete votes[`_`]
+      delete votes[`>`]
+
+      this.setState({
+        idx: nextIdx,
+        img: nextImg,
+        votes,
+      })
+    })
   }
   render () {
-    const {images} = this.props
-    const {idx} = this.state
+    const {idx, img, votes} = this.state
+
     return (
       <div>
+          <p>Gun score {JSON.stringify(votes)}</p>
           <h1 style={{ textAlign: 'center' }}>1: Fake, 2: Propaganda, 3: Campanha, 4: Haddad</h1>
-          <img src={`/images/${images[idx]}`}/>
-          {JSON.stringify(idx)}
+          <img src={`/images/${img}`}/>
+
       </div>
     )
   }
---
+++ src/gun.js
@@ -0,0 +1,20 @@
+const Gun = require('gun/gun')
+const gun = Gun(['http://localhost:8080/gun'])
+
+const GUN_CLASS = 'opzapbozo2018'
+const GUN_URL = process.env.GUN_URL || 'http://localhost:8080/gun'
+const chain = gun.get(GUN_CLASS)
+
+const gunGet = (key) => new Promise((accept, reject) => gun.get(key).once(data =>
+    {
+        accept(data || {})
+    }
+))
+
+export {
+    chain as default,
+    gunGet,
+    GUN_URL,
+    GUN_CLASS
+}
+
