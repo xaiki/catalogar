39473a2705729b640cf7b88783b2f778c1706456
Niv Sardi <xaiki@evilgiggle.com>
2018-10-19 12:18:38 -0300

    add react-keydown
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import { withSiteData } from 'react-static'
+import keydown from 'react-keydown'
 //
-import logoImg from '../logo.png'
 
-export default withSiteData(() => (
-  <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
-  </div>
-))
+class Main extends React.PureComponent {
+  state = {
+    idx: 0
+  }
+  @keydown('1', '2', '3', '4')
+  onKeyPress({key}) {
+    console.error('keypress', key)
+  }
+  render () {
+    return (
+      <div>
+          <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
+          {JSON.stringify(this.state)}
+      </div>
+    )
+  }
+}
+export default withSiteData(Main)
