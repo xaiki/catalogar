efc1b0d53d1e1599112d7a4ed655d3bddd458678
Niv Sardi <xaiki@evilgiggle.com>
2018-10-22 09:41:48 -0300

    actually use GUN_URL
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/gun.js
+++ src/gun.js
@@ -1,8 +1,8 @@
 const Gun = require('gun/gun')
-const gun = Gun(['http://localhost:8080/gun'])
 
 const GUN_CLASS = 'opzapbozo2018'
 const GUN_URL = process.env.GUN_URL || 'http://localhost:8080/gun'
+const gun = Gun([GUN_URL])
 const chain = gun.get(GUN_CLASS)
 
 const gunGet = (key) => new Promise((accept, reject) => gun.get(key).once(data =>
