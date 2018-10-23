50645f2f0ceafce6a274e10bf26b65150e5df71c
Niv Sardi <xaiki@evilgiggle.com>
2018-10-23 16:31:49 -0300

    add search
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- imgtop.js
+++ imgtop.js
@@ -5,27 +5,31 @@ const file = process.argv.pop()
 const type = process.argv.pop()
 const data = {}
 
+const search = process.env.CT_SEARCH || NULL
+let found
 
 const reader = readline.createInterface({
     input: fs.createReadStream(file)
 })
 
-
 reader.on('line', line => {
     const e = JSON.parse(line)
     if (e.message_type !== type) return
     const key = e.content
+
+    const ext = e.media.mime.split('/').pop()
+    const timestamp = e.timestamp['$date']
+    const filename = `${type}s/${timestamp}${e.sender_id}.${ext}`
+
     if (data[key]) {
         data[key].score++
         data[key].senders[e.sender_id] = data[key].senders[e.sender_id] ? data[key].senders[e.sender_id] + 1 : 1
         data[key].captions[e.media.caption] = data[key].captions[e.media.caption] ? data[key].captions[e.media.caption] + 1 : 1
     } else {
-        const ext = e.media.mime.split('/').pop()
-        const timestamp = e.timestamp['$date']
         data[key] = {
-            filename: `${type}s/${timestamp}${e.sender_id}.${ext}`,
+            filename: filename,
             score: 1,
-            senders: {
+                  senders: {
                 [e.sender_id]: 1
             },
             captions: {
@@ -33,11 +37,20 @@ reader.on('line', line => {
             }
         }
     }
+
+    if (!found && filename === search) {
+        found = key
+    }
 })
 
 const s2s = (senders) => Object.entries(senders).map(e => e.join(':')).join(',')
 
 reader.on('close', () => {
-    const res = Object.values(data).sort((a, b) => b.score - a.score)
-    res.map(e => console.log(`${e.filename}\t${e.score}\t${s2s(e.senders)}`))
+    if (found) {
+        console.log('FOUND', data[found])
+    } else {
+        if (search) return console.log('NOT FOUND', search)
+        const res = Object.values(data).sort((a, b) => b.score - a.score)
+        res.map(e => console.log(`${e.filename}\t${e.score}\t${s2s(e.senders)}`))
+    }
 })
