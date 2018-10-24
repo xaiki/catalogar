d8f787d93eed95260445c06fab5ce67bbb58fa41
Niv Sardi <xaiki@evilgiggle.com>
2018-10-24 13:51:12 -0300

    add apollo host
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/connectors/apollo.js
+++ src/connectors/apollo.js
@@ -3,10 +3,12 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'node-fetch'
 
+const APOLLO_HOST = process.env.APOLLO_HOST || 'localhost'
+
 const client = new ApolloClient({
     link: new HttpLink({
         fetch,
-        uri: 'http://localhost:5000',
+        uri: `http://${APOLLO_HOST}:5000`,
     }),
     cache: new InMemoryCache(),
 })
