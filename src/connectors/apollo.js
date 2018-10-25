d8c06f498caa5f9899560c5e14d6bab8477a0ad8
Niv Sardi <xaiki@evilgiggle.com>
2018-10-25 10:31:04 -0300

    fix for node-fetch
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/connectors/apollo.js
+++ src/connectors/apollo.js
@@ -3,7 +3,12 @@ import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import fetch from 'node-fetch'
 
-const APOLLO_URL = process.env.APOLLO_URL || '/graphql'
+let APOLLO_URL
+try {
+    APOLLO_URL = process.env.APOLLO_URL || location.origin + '/graphql'
+} catch (e) {
+    APOLLO_URL = '/graphql'
+}
 
 const client = new ApolloClient({
     link: new HttpLink({
