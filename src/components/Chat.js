d0167eac0e13e6100909409aecff0b297bc918ff
Niv Sardi <xaiki@evilgiggle.com>
2018-10-26 18:13:45 -0300

    fix chat media links
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,23 +6,23 @@ const Chat = ({ src, group, body, caption, timestamp, preview, filename = ''}) =
     const isMedia = filename.match(/.(mp4|jpe)/)
     return (
         <div className="msg">
-        <div className="bubble">
-        <div className="txt">
-        <span className="name">+{src}<span> ~ {group}</span></span>
-        <span className="timestamp">{timestamp}</span>
-        <p className="message">{caption ? caption : body}</p>
-        </div>
-        <div className="bubble-arrow"></div>
-        </div>
-        {isMedia && (
             <div className="bubble">
-            <a className="media" href={filename}>
-            {caption && <p className="message">{caption}</p>}
-            <img src={`data:image/jpeg;base64,${preview}`} />
-                         </a>
-                         <div className="bubble-arrow"></div>
-        </div>
-        )}
+                <div className="txt">
+                    <span className="name">+{src}<span> ~ {group}</span></span>
+                    <span className="timestamp">{timestamp}</span>
+                    <p className="message">{caption ? caption : body}</p>
+                </div>
+                <div className="bubble-arrow"></div>
+            </div>
+            {isMedia && (
+                 <div className="bubble">
+                     <a className="media" href={`/wppmon/${filename}`}>
+                         {caption && <p className="message">{caption}</p>}
+                         <img src={`data:image/jpeg;base64,${preview}`} />
+                     </a>
+                     <div className="bubble-arrow"></div>
+                 </div>
+            )}
         </div>
     )
 }
