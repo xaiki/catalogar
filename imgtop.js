5cb16d6e709322972ebc2636e6a071636e332970
Niv Sardi <xaiki@evilgiggle.com>
2018-10-27 09:49:24 -0300

    make top an util
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- imgtop.js
+++ imgtop.js
@@ -1,54 +1,21 @@
 const fs = require('fs')
 const readline = require('readline')
-const { makeFilename } = require('./src/utils')
+const { makeFilename, Top } = require('./src/utils')
 
+const top = new Top()
 const file = process.argv.pop()
-const type = process.argv.pop()
-const data = {}
-
-const search = process.env.CT_SEARCH || null
-let found
 
 const reader = readline.createInterface({
     input: fs.createReadStream(file)
 })
 
 reader.on('line', line => {
-    const e = JSON.parse(line)
-    if (e.message_type !== type) return
-    const key = e.content
-    const filename = makeFilename(e)
-
-    if (data[key]) {
-        data[key].score++
-        data[key].senders[e.sender_id] = data[key].senders[e.sender_id] ? data[key].senders[e.sender_id] + 1 : 1
-        data[key].captions[e.media.caption] = data[key].captions[e.media.caption] ? data[key].captions[e.media.caption] + 1 : 1
-    } else {
-        data[key] = {
-            filename: filename,
-            score: 1,
-            senders: {
-                [e.sender_id]: 1
-            },
-            captions: {
-                [e.media.caption]: 1
-            }
-        }
-    }
-
-    if (!found && filename === search) {
-        found = key
-    }
+    top.add(JSON.parse(line))
 })
 
 const s2s = (senders) => Object.entries(senders).map(e => e.join(':')).join(',')
 
 reader.on('close', () => {
-    if (found) {
-        console.log('FOUND', data[found])
-    } else {
-        if (search) return console.log('NOT FOUND', search)
-        const res = Object.values(data).sort((a, b) => b.score - a.score)
-        res.map(e => console.log(`${e.filename}\t${e.score}\t${s2s(e.senders)}`))
-    }
+    const res = top.sort()
+    res.map(e => console.log(`${e.filename}\t${e.score}\t${s2s(e.senders)}\t${s2s(e.groups)}`))
 })
--- src/utils.js
+++ src/utils.js
@@ -7,4 +7,52 @@ const makeFilename = e => {
     return `${type}s/${date}_${src}_${file}`
 }
 
-module.exports = { makeFilename }
+class Top {
+    constructor() {
+        this.data = {}
+    }
+    add(e) {
+        if (e.message_type === 'ciphertext') return
+
+        const key = ['audio', 'ptt', 'document']
+            .indexOf(e.message_type) === -1 ? e.content : e.media.link
+        const filename = makeFilename(e)
+
+        if (this.data[key]) {
+            this.data[key].score++
+            this.data[key].senders[e.sender_id] =
+                this.data[key].senders[e.sender_id] ?
+                this.data[key].senders[e.sender_id] + 1 : 1
+            this.data[key].groups[e.group_id] =
+                this.data[key].groups[e.group_id] ?
+                this.data[key].groups[e.group_id] + 1 : 1
+            this.data[key].captions[e.media.caption] =
+                this.data[key].captions[e.media.caption] ?
+                this.data[key].captions[e.media.caption] + 1 : 1
+        } else {
+            this.data[key] = {
+                filename: filename,
+                score: 1,
+                senders: {
+                    [e.sender_id]: 1
+                },
+                groups: {
+                    [e.group_id]: 1
+                },
+                captions: {
+                    [e.media.caption]: 1
+                }
+            }
+        }
+
+        return this.data[key]
+    }
+    values() {
+        return Object.values(this.data)
+    }
+    sort() {
+        return this.values().sort((a, b) => b.score - a.score)
+    }
+}
+
+module.exports = { makeFilename, Top }
