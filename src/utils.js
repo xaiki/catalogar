5e7699b07d91afff7494f726890e1053c9ec3a8f
Niv Sardi <xaiki@evilgiggle.com>
2018-10-28 14:38:14 -0300

    s/key/hash/
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/utils.js
+++ src/utils.js
@@ -35,29 +35,31 @@ class Top {
 
         const key = ['audio', 'ptt', 'document']
             .indexOf(e.message_type) === -1 ? e.content : e.media.link
-        const filename = makeFilename(e)
+        const hash = md5(key)
+        const filename = makeFilename(e, hash)
 
-        if (this.data[key]) {
-            this.data[key].total++
-            this.data[key].senders[e.sender_id] =
-                this.data[key].senders[e.sender_id] ?
-                this.data[key].senders[e.sender_id] + 1 : 1
-            this.data[key].groups[e.chat_id] =
-                this.data[key].groups[e.chat_id] ?
-                this.data[key].groups[e.chat_id] + 1 : 1
-            this.data[key].captions[e.media.caption] =
-                this.data[key].captions[e.media.caption] ?
-                this.data[key].captions[e.media.caption] + 1 : 1
+        if (this.data[hash]) {
+            this.data[hash].total++
+            this.data[hash].senders[e.sender_id] =
+                this.data[hash].senders[e.sender_id] ?
+                this.data[hash].senders[e.sender_id] + 1 : 1
+            this.data[hash].groups[e.chat_id] =
+                this.data[hash].groups[e.chat_id] ?
+                this.data[hash].groups[e.chat_id] + 1 : 1
+            this.data[hash].captions[e.media.caption] =
+                this.data[hash].captions[e.media.caption] ?
+                this.data[hash].captions[e.media.caption] + 1 : 1
 
-            if (this.data[key].filenames[e.media.filename]) {
+            if (this.data[hash].filenames[e.media.filename]) {
                 const fn = this.filenames[e.media.filename]
-                this.data[key].filenames[fn] = this.data[key].filenames[fn] + 1
+                this.data[hash].filenames[fn] = this.data[hash].filenames[fn] + 1
             } else {
-                this.data[key].filenames[filename] = 1
+                this.data[hash].filenames[filename] = 1
             }
         } else {
-            this.data[key] = {
+            this.data[hash] = {
                 filename: filename,
+                hash: hash,
                 total: 1,
                 senders: {
                     [e.sender_id]: 1
@@ -74,7 +76,7 @@ class Top {
             }
         }
 
-        return key
+        return hash
     }
     values() {
         return Object.values(this.data)
@@ -82,8 +84,8 @@ class Top {
     sort() {
         return this.values().sort((a, b) => b.total - a.total)
     }
-    get(key) {
-        return this.data[key]
+    get(hash) {
+        return this.data[hash]
     }
 }
 
